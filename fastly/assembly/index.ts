8e03f79a4a6ed08b870ab126ac6a92ce06941d1b
Lars Trieloff <trieloff@adobe.com>
2020-11-02 09:41:49 +0000

    style(index): apply eslint rules from assemblyscript project

--- fastly/assembly/index.ts
+++ fastly/assembly/index.ts
@@ -1,20 +1,20 @@
 import { Request,  Response, Fastly, Headers } from "@fastly/as-compute";
 
 function getQueryParam(qs: string, param: string): string {
-    const pairs = qs.split("&");
-    for (let i = 0; i < pairs.length; i++) {
-        if (pairs[i].indexOf(param + "=")==0) {
-            return pairs[i].substr(param.length + 1);
-        }
+  const pairs = qs.split("&");
+  for (let i = 0; i < pairs.length; i++) {
+    if (pairs[i].indexOf(param + "=")==0) {
+      return pairs[i].substr(param.length + 1);
     }
-    return "";
+  }
+  return "";
 }
 
 function getQueryString(path: string):string {
-    if (path.indexOf("?") > 0) {
-        return path.substring(path.indexOf("?") + 1);
-    }
-    return "";
+  if (path.indexOf("?") > 0) {
+    return path.substring(path.indexOf("?") + 1);
+  }
+  return "";
 }
 
 // The entry point for your application.
@@ -24,83 +24,83 @@ function getQueryString(path: string):string {
 // the request to a backend, make completely new requests, and/or generate
 // synthetic responses.
 function main(req: Request): Response {
-    // Make any desired changes to the client request.
-    req.headers().set("Host", "example.com");
+  // Make any desired changes to the client request.
+  req.headers().set("Host", "example.com");
 
-    // We can filter requests that have unexpected methods.
-    const VALID_METHODS = ["HEAD", "GET", "POST"];
-    if (!VALID_METHODS.includes(req.method())) {
-        return new Response(String.UTF8.encode("This method is not allowed"), {
-            status: 405,
-        });
-    }
-
-    let urlParts = req.url().split("//").pop().split("/");
-    let path = ("/" + urlParts.join("/"));
-    let qs = getQueryString(path);
-
-    const owner = getQueryParam(qs, "owner");
-    const repo = getQueryParam(qs, "repo"); 
-    let ref = getQueryParam(qs, "ref");
-    let sha = "";
-
-    if (owner != "" && repo != "" && true) {
-        let cacheOverride = new Fastly.CacheOverride();
-        cacheOverride.setTTL(30);
-
-        const myreq = new Request("https://github.com/" + owner + "/" + repo + ".git/info/refs?service=git-upload-pack", {});
-
-        const myresp = Fastly.fetch(myreq, {
-            backend: "GitHub",
-            cacheOverride,
-        }).wait();
-
-        if (myresp.status() >= 400 && myresp.status() < 500) {
-            return new Response(String.UTF8.encode('failed to fetch git repo info (statusCode: ' + myresp.status().toString(10) +', statusMessage: ' + myresp.statusText() + ')'), {
-                status: 404
-            });
-        }
-
-        if (myresp.status() >= 500) {
-            return new Response(String.UTF8.encode('failed to fetch git repo info (statusCode: ' + myresp.status().toString(10) +', statusMessage: ' + myresp.statusText() + ')'), {
-                status: 502 // bad gateway
-            });
-        }
-
-        const lines = myresp.text().split("\n");
-        for (let i = 0; i < lines.length; i++) {
-            if (ref == "" && lines[i].indexOf("symref=HEAD:") > 0) {
-                let refline = lines[i].substr(lines[i].indexOf("symref=HEAD:") + 23);
-                ref = refline.substr(0, refline.indexOf(" "));
-            }
-            if (ref != "" && sha == "" && lines[i].indexOf(" refs/heads/" + ref) > 0) {
-                sha = lines[i].substr(0, lines[i].indexOf(" refs/heads/" + ref));
-                ref = "refs/heads/" + ref;
-            }
-            if (ref != "" && sha == "" && lines[i].indexOf(" refs/tags/" + ref) > 0) {
-                sha = lines[i].substr(0, lines[i].indexOf(" refs/tags/" + ref));
-                ref = "refs/tags/" + ref;
-            }
-        }
-
-        if (sha == "") {
-            return new Response(String.UTF8.encode('ref not found'), {
-                status: 404
-            });
-        }
-
-        const myheaders = new Headers();
-        myheaders.set("Content-Type", "application/json");
-
-        return new Response(String.UTF8.encode('{ "fqRef": "' + ref + '", "sha": "' + sha + '"  }'), {
-            status: 200,
-            headers: myheaders
-          });
-    }
-
-    return new Response(String.UTF8.encode('owner and repo are mandatory parameters!'), {
-        status: 400
+  // We can filter requests that have unexpected methods.
+  const VALID_METHODS = ["HEAD", "GET", "POST"];
+  if (!VALID_METHODS.includes(req.method())) {
+    return new Response(String.UTF8.encode("This method is not allowed"), {
+      status: 405,
     });
+  }
+
+  let urlParts = req.url().split("//").pop().split("/");
+  let path = ("/" + urlParts.join("/"));
+  let qs = getQueryString(path);
+
+  const owner = getQueryParam(qs, "owner");
+  const repo = getQueryParam(qs, "repo"); 
+  let ref = getQueryParam(qs, "ref");
+  let sha = "";
+
+  if (owner != "" && repo != "" && true) {
+    let cacheOverride = new Fastly.CacheOverride();
+    cacheOverride.setTTL(30);
+
+    const myreq = new Request("https://github.com/" + owner + "/" + repo + ".git/info/refs?service=git-upload-pack", {});
+
+    const myresp = Fastly.fetch(myreq, {
+      backend: "GitHub",
+      cacheOverride,
+    }).wait();
+
+    if (myresp.status() >= 400 && myresp.status() < 500) {
+      return new Response(String.UTF8.encode('failed to fetch git repo info (statusCode: ' + myresp.status().toString(10) +', statusMessage: ' + myresp.statusText() + ')'), {
+        status: 404
+      });
+    }
+
+    if (myresp.status() >= 500) {
+      return new Response(String.UTF8.encode('failed to fetch git repo info (statusCode: ' + myresp.status().toString(10) +', statusMessage: ' + myresp.statusText() + ')'), {
+        status: 502 // bad gateway
+      });
+    }
+
+    const lines = myresp.text().split("\n");
+    for (let i = 0; i < lines.length; i++) {
+      if (ref == "" && lines[i].indexOf("symref=HEAD:") > 0) {
+        let refline = lines[i].substr(lines[i].indexOf("symref=HEAD:") + 23);
+        ref = refline.substr(0, refline.indexOf(" "));
+      }
+      if (ref != "" && sha == "" && lines[i].indexOf(" refs/heads/" + ref) > 0) {
+        sha = lines[i].substr(0, lines[i].indexOf(" refs/heads/" + ref));
+        ref = "refs/heads/" + ref;
+      }
+      if (ref != "" && sha == "" && lines[i].indexOf(" refs/tags/" + ref) > 0) {
+        sha = lines[i].substr(0, lines[i].indexOf(" refs/tags/" + ref));
+        ref = "refs/tags/" + ref;
+      }
+    }
+
+    if (sha == "") {
+      return new Response(String.UTF8.encode('ref not found'), {
+        status: 404
+      });
+    }
+
+    const myheaders = new Headers();
+    myheaders.set("Content-Type", "application/json");
+
+    return new Response(String.UTF8.encode('{ "fqRef": "' + ref + '", "sha": "' + sha + '"  }'), {
+      status: 200,
+      headers: myheaders
+    });
+  }
+
+  return new Response(String.UTF8.encode('owner and repo are mandatory parameters!'), {
+    status: 400
+  });
 }
 
 // Get the request from the client.
